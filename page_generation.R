b587a0b9c2b98558346f9831fe03c31277e6df9e
George Moroz <agricolamz@gmail.com>
2023-02-08 18:09:10 +0300

    first commit

---
+++ page_generation.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(glue)
+
+map(list.files("texts"), function(i){
+  
+  readxl::read_xlsx(str_c("texts/", i)) %>% 
+    mutate_all(function(x){ifelse(is.na(x), "", x)}) ->
+    df
+  
+  file_name <- unique(df$file_name)
+  
+  "
+  ```{r setup, include=FALSE}
+  knitr::opts_chunk$set(echo = FALSE)
+  library(tippy)
+  ```
+  " %>% 
+    write_lines(str_c(file_name, ".Rmd"))
+  
+  str_c("## ", unique(df$doc_id), "\n\n::: {dir=rtl}\n\n") %>% 
+    write_lines(str_c(file_name, ".Rmd"), append = TRUE)
+  
+  df %>% 
+    fill(paragraph_id, sentence_id, sentence, term_id, token_id) %>% 
+    mutate(tooltip = str_c(lemma, "<br>", upos,"<br>", feats)) %>% 
+    group_by(paragraph_id, sentence_id, sentence, term_id, token_id, token) %>% 
+    summarise(tooltip = str_c(tooltip, collapse = "<br>")) %>% 
+    glue_data('`r tippy("{token}", tooltip = "{tooltip}")`')  %>% 
+    write_lines(str_c(file_name, ".Rmd"), append = TRUE)
+  
+  ":::\n\n" %>% 
+    write_lines(str_c(file_name, ".Rmd"), append = TRUE)
+  
+  "## Словарь\n\n::: {dir=rtl}\n\n" %>% 
+    write_lines(str_c(file_name, ".Rmd"), append = TRUE)
+  
+  str_c(df$lemma, df$meaning) %>% 
+    unique() %>%
+    sort() %>% 
+    str_c(collapse = "; ") %>% 
+    write_lines(str_c(file_name, ".Rmd"), append = TRUE)
+  
+  ":::\n\n" %>% 
+    write_lines(str_c(file_name, ".Rmd"), append = TRUE)
+})
